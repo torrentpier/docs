22064d14b0d7252c6a59e7cce3f80f8cfef7db44
Roman Kelesidis <roman25052006.kelesh@gmail.com>
2024-08-01 14:34:14 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,95 +8,107 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'TorrentPier',
-  tagline: 'Bull-powered BitTorrent tracker engine',
-  favicon: 'img/favicon.ico',
+    title: 'TorrentPier',
+    tagline: 'Bull-powered BitTorrent tracker engine',
+    favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://torrentpier.com/',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+    // Set the production url of your site here
+    url: 'https://torrentpier.com/',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'torrentpier', // Usually your GitHub org/user name.
-  projectName: 'torrentpier', // Usually your repo name.
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'torrentpier', // Usually your GitHub org/user name.
+    projectName: 'torrentpier', // Usually your repo name.
 
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+    onBrokenLinks: 'warn',
+    onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/torrentpier/docs/tree/master/',
-        },
-        /*blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },*/
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
-      navbar: {
-        title: 'TorrentPier',
-        logo: {
-          alt: 'TorrentPier Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Docs',
-          },
-          {to: 'https://torrentpier.duckdns.org', label: 'Demo version', position: 'left'},
-          {to: 'https://get-torrentpier.duckdns.org', label: 'Get TorrentPier', position: 'left'},
-          {to: 'https://torrentpier.com', label: 'Our Forum', position: 'left'},
-          {
-            href: 'https://github.com/torrentpier/torrentpier',
-            label: 'GitHub',
-            position: 'right',
-          },
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: './sidebars.js',
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/torrentpier/docs/tree/master/',
+                },
+                /*blog: {
+                  showReadingTime: true,
+                  // Please change this to your repo.
+                  // Remove this to remove the "edit this page" links.
+                  editUrl:
+                    'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                },*/
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            }),
         ],
-      },
-      footer: {
-        style: 'light',
-        copyright: `Copyright © ${new Date().getFullYear()} TorrentPier. Built with ❤️ and Docusaurus 🦖.`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+    ],
+
+    themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            // Replace with your project's social card
+            image: 'img/docusaurus-social-card.jpg',
+            navbar: {
+                title: 'TorrentPier',
+                logo: {
+                    alt: 'TorrentPier Logo',
+                    src: 'img/logo.svg',
+                },
+                items: [
+                    {
+                        type: 'docSidebar',
+                        sidebarId: 'tutorialSidebar',
+                        position: 'left',
+                        label: 'Docs',
+                    },
+                    {
+                        href: 'https://torrentpier.duckdns.org',
+                        label: 'Demo version',
+                        position: 'right'
+                    },
+                    {
+                        href: 'https://get-torrentpier.duckdns.org',
+                        label: 'Get TorrentPier',
+                        position: 'right'
+                    },
+                    {
+                        href: 'https://torrentpier.com',
+                        label: 'Our Forum',
+                        position: 'right'
+                    },
+                    {
+                        href: 'https://github.com/torrentpier/torrentpier',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                ],
+            },
+            footer: {
+                style: 'light',
+                copyright: `Copyright © ${new Date().getFullYear()} TorrentPier. Built with ❤️ and Docusaurus 🦖.`,
+            },
+            prism: {
+                theme: prismThemes.github,
+                darkTheme: prismThemes.dracula,
+            },
+        }),
 };
 
 export default config;
